3f376a67889ceba8e7f791f5c320dc9509006eb8
Jose Bojorquez <u22311398@utp.edu.pe>
2025-10-29 23:18:53 -0500

    Modificaciones y implmentaciones de las logicas

---
+++ src/main/java/com/example/SeleccionUniversitaria/Controller/VistaController.java
@@ -0,0 +1,17 @@
+package com.example.SeleccionUniversitaria.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VistaController {
+    @GetMapping("/")
+    public String index() {
+        return "index"; 
+    }
+
+    @GetMapping("/TestVocacional")
+    public String testVocacional() {
+        return "TestVocacional"; 
+    }
+}
