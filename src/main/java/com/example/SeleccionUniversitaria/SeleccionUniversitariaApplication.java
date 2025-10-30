b189a189e615f936744d6e648b243acbe8504874
Jose Bojorquez <u22311398@utp.edu.pe>
2025-10-29 22:26:20 -0500

    commit del .gitignore

---
+++ src/main/java/com/example/SeleccionUniversitaria/SeleccionUniversitariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SeleccionUniversitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleccionUniversitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleccionUniversitariaApplication.class, args);
+	}
+
+}
